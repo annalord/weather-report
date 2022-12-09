c77e29ee3f65cf45a47e831a0cd1e2f2e2cd0e6a
Anna <annalordkemp@gmail.com>
2022-12-09 13:48:58 -0700

    adds behavior for changing temp with up and down arrows, adds behavior for changing temp background with temp changes, adds behavior for landscape change with temp change

--- src/index.js
+++ src/index.js
@@ -1,1 +1,52 @@
 'use strict';
+
+const state = {
+  temp: 75,
+};
+
+const increaseTemp = () => {
+  state.temp += 1;
+  const bigTempContainer = document.querySelector('#big_temp');
+  bigTempContainer.textContent = `${state.temp}`;
+
+  changeTempColor(bigTempContainer);
+};
+
+const decreaseTemp = () => {
+  state.temp -= 1;
+  const bigTempContainer = document.querySelector('#big_temp');
+  bigTempContainer.textContent = `${state.temp}`;
+
+  changeTempColor(bigTempContainer);
+};
+
+const changeTempColor = (bigTempContainer) => {
+  const landscape = document.querySelector('#landscape');
+
+  if (state.temp > 80) {
+    bigTempContainer.className = 'hot';
+    landscape.textContent = '🌵__🐍_🦂_🌵🌵__🐍_🏜_🦂';
+  } else if (state.temp > 70) {
+    bigTempContainer.className = 'warm';
+    landscape.textContent = '🌸🌿🌼__🌷🌻🌿_☘️🌱_🌻🌷';
+  } else if (state.temp > 60) {
+    bigTempContainer.className = 'moderate';
+    landscape.textContent = '🌾🌾_🍃_🪨__🛤_🌾🌾🌾_🍃';
+  } else if (state.temp > 50) {
+    bigTempContainer.className = 'cold';
+    landscape.textContent = '🌲🌲⛄️🌲⛄️🍂🌲🍁🌲🌲⛄️🍂🌲';
+  } else {
+    bigTempContainer.className = 'very-cold';
+    landscape.textContent = '🏔❄️☃️⛷🏔❄️☃️⛷🏔❄️☃️⛷';
+  }
+};
+
+const registerEventHandlers = (event) => {
+  const upButton = document.querySelector('#up_arrow');
+  upButton.addEventListener('click', increaseTemp);
+
+  const downButton = document.querySelector('#down_arrow');
+  downButton.addEventListener('click', decreaseTemp);
+};
+
+document.addEventListener('DOMContentLoaded', registerEventHandlers);
